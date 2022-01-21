38bba4396691154e785fbc4ac095adf6adbed6b1
Sebastian Correa <correa.sebas6@gmail.com>
2022-01-21 14:29:02 -0500

    setting up app design

---
+++ app/src/main/java/com/example/nycrestaurants/AddRestaurantActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nycrestaurants
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddRestaurantActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_restaurant)
+    }
+}
