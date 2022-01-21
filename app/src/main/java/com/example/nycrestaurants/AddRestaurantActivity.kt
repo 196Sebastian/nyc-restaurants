81b79996dbe38953c31f9b3034b9e0c492347f2a
Sebastian Correa <correa.sebas6@gmail.com>
2022-01-21 17:29:16 -0500

    added dimens, strings, & colors

--- app/src/main/java/com/example/nycrestaurants/AddRestaurantActivity.kt
+++ app/src/main/java/com/example/nycrestaurants/AddRestaurantActivity.kt
@@ -2,10 +2,19 @@ package com.example.nycrestaurants
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
 
 class AddRestaurantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_restaurant)
+        setSupportActionBar(findViewById(R.id.toolbar_add_place))
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        findViewById<Toolbar>(R.id.toolbar_add_place).setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/example/nycrestaurants/MainActivity.kt
+++ app/src/main/java/com/example/nycrestaurants/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.nycrestaurants
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<FloatingActionButton>(R.id.fab_add_restaurant).setOnClickListener {
+            val intent = Intent(this, AddRestaurantActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
