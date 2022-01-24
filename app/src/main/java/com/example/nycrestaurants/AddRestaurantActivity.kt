037a0553b72f3f6ad8f62221c76894179607c492
Sebastian Correa <correa.sebas6@gmail.com>
2022-01-24 08:15:09 -0500

    using dexter added permission handling

--- app/src/main/java/com/example/nycrestaurants/AddRestaurantActivity.kt
+++ app/src/main/java/com/example/nycrestaurants/AddRestaurantActivity.kt
@@ -1,11 +1,24 @@
 package com.example.nycrestaurants
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -35,6 +48,7 @@ class AddRestaurantActivity : AppCompatActivity(), View.OnClickListener{
         }
 
         findViewById<EditText>(R.id.et_date).setOnClickListener(this)
+        findViewById<TextView>(R.id.tv_add_image).setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -45,7 +59,53 @@ class AddRestaurantActivity : AppCompatActivity(), View.OnClickListener{
                     calendar.get(Calendar.MONTH),
                     calendar.get(Calendar.DAY_OF_MONTH)).show()
             }
+            R.id.tv_add_image ->{
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+
+                val pictureDialogItems = arrayOf("Select photo from Gallery", "Capture photo from camera")
+                pictureDialog.setItems(pictureDialogItems){
+                        _, which ->
+                    when(which){
+                        0 -> choosePhotoFromGallery()
+                        1 -> Toast.makeText(this@AddRestaurantActivity, "Camera selection coming soon...", Toast.LENGTH_LONG).show()
+                    }
+                }
+                pictureDialog.show()
+            }
         }
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withContext(this).withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .withListener(object: MultiplePermissionsListener{
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?){
+                    if(report!!.areAllPermissionsGranted()){
+                        Toast.makeText(this@AddRestaurantActivity,
+                            "Storage READ/WRITE permission are granted. Now you can select an image from GALLERY", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>?, token: PermissionToken) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread().check()
+    }
+
+    private fun showRationalDialogForPermissions(){
+        AlertDialog.Builder(this).setMessage("It looks like you have turned off permission required for this feature. " +
+                "It can be enabled under the Applications Settings")
+            .setPositiveButton("GO TO SETTINGS"){ _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }.setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 
     private fun updateDateInView(){
