7f8455da02ea5eddd863e16700b70b2dc7c32516
Sebastian Correa <correa.sebas6@gmail.com>
2022-01-24 07:05:28 -0500

    added calendar picker

--- app/src/main/java/com/example/nycrestaurants/AddRestaurantActivity.kt
+++ app/src/main/java/com/example/nycrestaurants/AddRestaurantActivity.kt
@@ -1,11 +1,20 @@
 package com.example.nycrestaurants
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.appcompat.widget.Toolbar
+import java.text.SimpleDateFormat
+import java.util.*
 
 
-class AddRestaurantActivity : AppCompatActivity() {
+class AddRestaurantActivity : AppCompatActivity(), View.OnClickListener{
+
+    private var calendar = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_restaurant)
@@ -16,5 +25,35 @@ class AddRestaurantActivity : AppCompatActivity() {
         findViewById<Toolbar>(R.id.toolbar_add_place).setNavigationOnClickListener {
             onBackPressed()
         }
+
+        dateSetListener = DatePickerDialog.OnDateSetListener {
+                view, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+
+        findViewById<EditText>(R.id.et_date).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.et_date ->{
+                DatePickerDialog(this@AddRestaurantActivity, dateSetListener,
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        }
+    }
+
+    private fun updateDateInView(){
+
+        val myFormat = "MM.dd.yyyy"
+        val simpleDateFormat = SimpleDateFormat(myFormat, Locale.getDefault())
+
+        findViewById<EditText>(R.id.et_date).setText(simpleDateFormat.format(calendar.time).toString())
+
     }
 }
