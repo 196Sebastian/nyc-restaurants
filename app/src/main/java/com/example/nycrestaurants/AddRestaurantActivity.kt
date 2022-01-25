8a3c60a29018cf782b756079f6d7697501e678c2
Sebastian Correa <correa.sebas6@gmail.com>
2022-01-25 08:17:19 -0500

    updated code to get to your gallery

--- app/src/main/java/com/example/nycrestaurants/AddRestaurantActivity.kt
+++ app/src/main/java/com/example/nycrestaurants/AddRestaurantActivity.kt
@@ -22,11 +22,13 @@ import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import org.w3c.dom.Text
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
@@ -36,40 +38,51 @@ class AddRestaurantActivity : AppCompatActivity(), View.OnClickListener{
 
     private var calendar = Calendar.getInstance()
     private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
-    private lateinit var galleryImageResultLauncher: ActivityResultLauncher<Intent>
-    private lateinit var resultLauncherCamera: ActivityResultLauncher<Intent>
 
+    private val requestPermission: ActivityResultLauncher<Array<String>> = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions())
+    { permissions ->
+        run {
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if (isGranted) {
+                    Toast.makeText(
+                        this@AddRestaurantActivity,
+                        "Permission granted to read the storage files.",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    val pickIntent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
+                } else {
+                    if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        Toast.makeText(
+                            this@AddRestaurantActivity,
+                            "Oops you just denied the permission.",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+        }
+    }
+
+
+        private var openGalleryLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                result ->
+            if(result.resultCode == RESULT_OK && result.data != null){
+                val data: Intent? = result.data
+                val imageBackGround: Bitmap = data!!.extras?.get("data") as Bitmap
+                findViewById<ImageView>(R.id.iv_place_image).setImageBitmap(imageBackGround)
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_restaurant)
         setSupportActionBar(findViewById(R.id.toolbar_add_place))
-
-        galleryImageResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-                result ->
-            if(result.resultCode == Activity.RESULT_OK && result.data != null) {
-                val data: Intent? = result.data
-                val contentURI = data?.data
-
-                try{
-                    val selectedImageBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
-                    findViewById<ImageView>(R.id.iv_place_image).setImageBitmap(selectedImageBitmap)
-                } catch (e: IOException){
-                    e.printStackTrace()
-                    Toast.makeText(this@AddRestaurantActivity, "Failed to load Image from GALLERY", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-
-        resultLauncherCamera = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                // There are no request codes
-                val data: Intent? = result.data
-
-                val thumbNail : Bitmap = data!!.extras?.get("data") as Bitmap
-                findViewById<ImageView>(R.id.iv_place_image).setImageBitmap(thumbNail)
-            }
-        }
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
@@ -86,9 +99,28 @@ class AddRestaurantActivity : AppCompatActivity(), View.OnClickListener{
         }
 
         findViewById<EditText>(R.id.et_date).setOnClickListener(this)
-        findViewById<TextView>(R.id.tv_add_image).setOnClickListener(this)
+        var tvImage: TextView = findViewById(R.id.tv_add_image)
+        tvImage.setOnClickListener {
+            requestStoragePermission()
+        }
     }
 
+    private fun requestStoragePermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+            showRationaleDialog("NYC Restaurants", "Needs to Access your External Storage")
+        }else{
+            requestPermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE))
+        }
+    }
+
+    private fun showRationaleDialog(title: String, message: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ -> dialog.dismiss() }
+        builder.create().show()
+    }
+    
     override fun onClick(v: View?) {
         when(v!!.id){
             R.id.et_date ->{
@@ -97,73 +129,7 @@ class AddRestaurantActivity : AppCompatActivity(), View.OnClickListener{
                     calendar.get(Calendar.MONTH),
                     calendar.get(Calendar.DAY_OF_MONTH)).show()
             }
-            R.id.tv_add_image ->{
-                val pictureDialog = AlertDialog.Builder(this)
-                pictureDialog.setTitle("Select Action")
-
-                val pictureDialogItems = arrayOf("Select photo from Gallery", "Capture photo from camera")
-                pictureDialog.setItems(pictureDialogItems){
-                        _, which ->
-                    when(which){
-                        0 -> choosePhotoFromGallery()
-                        1 -> takePhotoFromCamera()
-                    }
-                }
-                pictureDialog.show()
-            }
         }
-    }
-
-    private fun takePhotoFromCamera(){
-        Dexter.withContext(this).withPermissions(
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.CAMERA
-        ).withListener(object: MultiplePermissionsListener{
-            override fun onPermissionsChecked(report: MultiplePermissionsReport?){
-                if(report!!.areAllPermissionsGranted()){
-                    val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                    resultLauncherCamera.launch(cameraIntent)
-                }
-            }
-            override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>?, token: PermissionToken) {
-                showRationalDialogForPermissions()
-            }
-        }).onSameThread().check()
-    }
-
-    private fun choosePhotoFromGallery() {
-        Dexter.withContext(this).withPermissions(
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ).withListener(object: MultiplePermissionsListener{
-                override fun onPermissionsChecked(report: MultiplePermissionsReport?){
-                    if(report!!.areAllPermissionsGranted()){
-                        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                        galleryImageResultLauncher.launch(galleryIntent)
-                    }
-                }
-                override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>?, token: PermissionToken) {
-                    showRationalDialogForPermissions()
-                }
-            }).onSameThread().check()
-    }
-
-    private fun showRationalDialogForPermissions(){
-        AlertDialog.Builder(this).setMessage("It looks like you have turned off permission required for this feature. " +
-                "It can be enabled under the Applications Settings")
-            .setPositiveButton("GO TO SETTINGS"){ _, _ ->
-                try {
-                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                    val uri = Uri.fromParts("package", packageName, null)
-                    intent.data = uri
-                    startActivity(intent)
-                } catch (e: ActivityNotFoundException){
-                    e.printStackTrace()
-                }
-            }.setNegativeButton("Cancel") { dialog, _ ->
-                dialog.dismiss()
-            }.show()
     }
 
     private fun updateDateInView(){
@@ -174,7 +140,4 @@ class AddRestaurantActivity : AppCompatActivity(), View.OnClickListener{
         findViewById<EditText>(R.id.et_date).setText(simpleDateFormat.format(calendar.time).toString())
     }
 
-    companion object {
-        private const val GALLERY = 1
-    }
 }
