a9b263e259737cec089ef23ac09656ea3e17e1c1
Sebastian Correa <correa.sebas6@gmail.com>
2022-01-27 14:50:28 -0500

    added rounded images

--- app/src/main/java/com/example/nycrestaurants/activities/MainActivity.kt
+++ app/src/main/java/com/example/nycrestaurants/activities/MainActivity.kt
@@ -4,7 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.nycrestaurants.R
+import com.example.nycrestaurants.adapters.NYCRestaurantAdapter
 import com.example.nycrestaurants.database.DatabaseHandler
 import com.example.nycrestaurants.models.NYCRestaurantModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -23,15 +28,28 @@ class MainActivity : AppCompatActivity() {
         getNYCRestaurantListFromLocalDB()
     }
 
+    private fun setUpNYCRestaurantRecyclerView(nycRestaurantList: ArrayList<NYCRestaurantModel>) {
+        val rvList = findViewById<RecyclerView>(R.id.rv_nyc_restaurant_list)
+
+        rvList.setHasFixedSize(true)
+
+        rvList.layoutManager = LinearLayoutManager(this)
+        val restaurantAdapter = NYCRestaurantAdapter(this, nycRestaurantList)
+        rvList.adapter = restaurantAdapter
+    }
+
     private fun getNYCRestaurantListFromLocalDB(){
         val dbHandler = DatabaseHandler(this)
         val getNYCRestaurantList : ArrayList<NYCRestaurantModel> = dbHandler.getNYCRestaurantList()
 
         if(getNYCRestaurantList.size > 0){
-            for(i in getNYCRestaurantList){
-                Log.e("Title", i.title)
-                Log.e("Description", i.description)
-            }
+            findViewById<RecyclerView>(R.id.rv_nyc_restaurant_list).visibility = View.VISIBLE
+            findViewById<TextView>(R.id.tv_no_records_available).visibility = View.GONE
+
+            setUpNYCRestaurantRecyclerView(getNYCRestaurantList)
+        }else{
+            findViewById<RecyclerView>(R.id.rv_nyc_restaurant_list).visibility = View.GONE
+            findViewById<TextView>(R.id.tv_no_records_available).visibility = View.VISIBLE
         }
     }
 }
